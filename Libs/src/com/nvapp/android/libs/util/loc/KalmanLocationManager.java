1d2eb1d768f05b9496a38311f9019011093ea11e
lexloo@gmail.com <NC5Ct2Cu3ZZ5>
2015-01-10 21:41:36 +0800

    add kalman filter

---
+++ Libs/src/com/nvapp/android/libs/util/loc/KalmanLocationManager.java
@@ -0,0 +1,134 @@
+package com.nvapp.android.libs.util.loc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.location.LocationListener;
+import android.util.Log;
+
+public class KalmanLocationManager {
+
+	public enum UseProvider {
+		GPS, NET, GPS_AND_NET
+	}
+
+	public static final String KALMAN_PROVIDER = "kalman";
+
+	private static final String TAG = KalmanLocationManager.class.getSimpleName();
+
+	private final Context mContext;
+
+	private final Map<LocationListener, LooperThread> mListener2Thread;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param context
+	 *            The Context for this KalmanLocationManager.
+	 */
+	public KalmanLocationManager(Context context) {
+
+		mContext = context;
+		mListener2Thread = new HashMap<LocationListener, LooperThread>();
+	}
+
+	/**
+	 * Register for {@link android.location.Location Location} estimates using
+	 * the given LocationListener callback.
+	 * 
+	 * 
+	 * @param useProvider
+	 *            Specifies which of the native location providers to use, or a
+	 *            combination of them.
+	 * 
+	 * @param minTimeFilter
+	 *            Minimum time interval between location estimates, in
+	 *            milliseconds. Indicates the frequency of predictions to be
+	 *            calculated by the filter, thus the frequency of callbacks to
+	 *            be received by the given location listener.
+	 * 
+	 * @param minTimeGpsProvider
+	 *            Minimum time interval between GPS readings, in milliseconds.
+	 *            If {@link UseProvider#NET UseProvider.NET} was set, this value
+	 *            is ignored.
+	 * 
+	 * @param minTimeNetProvider
+	 *            Minimum time interval between Network readings, in
+	 *            milliseconds. If {@link UseProvider#GPS UseProvider.GPS} was
+	 *            set, this value is ignored.
+	 * 
+	 * @param listener
+	 *            A {@link android.location.LocationListener LocationListener}
+	 *            whose
+	 *            {@link android.location.LocationListener#onLocationChanged(android.location.Location)
+	 *            onLocationChanged(Location)} method will be called for each
+	 *            location estimate produced by the filter. It will also receive
+	 *            the status updates from the native providers.
+	 * 
+	 * @param forwardProviderReadings
+	 *            Also forward location readings from the native providers to
+	 *            the given listener. Note that <i>status</i> updates will
+	 *            always be forwarded.
+	 * 
+	 */
+	public void requestLocationUpdates(UseProvider useProvider, long minTimeFilter, long minTimeGpsProvider,
+			long minTimeNetProvider, LocationListener listener, boolean forwardProviderReadings) {
+		// Validate arguments
+		if (useProvider == null)
+			throw new IllegalArgumentException("useProvider can't be null");
+
+		if (listener == null)
+			throw new IllegalArgumentException("listener can't be null");
+
+		if (minTimeFilter < 0) {
+			Log.w(TAG, "minTimeFilter < 0. Setting to 0");
+			minTimeFilter = 0;
+		}
+
+		if (minTimeGpsProvider < 0) {
+
+			Log.w(TAG, "minTimeGpsProvider < 0. Setting to 0");
+			minTimeGpsProvider = 0;
+		}
+
+		if (minTimeNetProvider < 0) {
+
+			Log.w(TAG, "minTimeNetProvider < 0. Setting to 0");
+			minTimeNetProvider = 0;
+		}
+
+		// Remove this listener if it is already in use
+		if (mListener2Thread.containsKey(listener)) {
+
+			Log.d(TAG, "Requested location updates with a listener that is already in use. Removing.");
+			removeUpdates(listener);
+		}
+
+		LooperThread looperThread = new LooperThread(mContext, useProvider, minTimeFilter, minTimeGpsProvider,
+				minTimeNetProvider, listener, forwardProviderReadings);
+
+		mListener2Thread.put(listener, looperThread);
+	}
+
+	/**
+	 * Removes location estimates for the specified LocationListener.
+	 * <p>
+	 * Following this call, updates will no longer occur for this listener.
+	 * 
+	 * @param listener
+	 *            Listener object that no longer needs location estimates.
+	 */
+	public void removeUpdates(LocationListener listener) {
+
+		LooperThread looperThread = mListener2Thread.remove(listener);
+
+		if (looperThread == null) {
+
+			Log.d(TAG, "Did not remove updates for given LocationListener. Wasn't registered in this instance.");
+			return;
+		}
+
+		looperThread.close();
+	}
+}
---
+++ Libs/src/com/nvapp/android/libs/util/loc/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 定位相关
+ */
+package com.nvapp.android.libs.util.loc;
