c706f6bbdc8f2ac4b6b3a726222fde60257d2566
lexloo <lexloo@126.com>
2015-03-15 17:17:11 +0800

    in progress

--- Libs/src/com/nvapp/form/ItemView.java
+++ Libs/src/com/nvapp/form/ItemView.java
@@ -1,21 +1,85 @@
 package com.nvapp.form;
 
-import android.annotation.SuppressLint;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class ItemView extends LinearLayout {
-	public ItemView(Context context) {
-		super(context);
-	}
+public abstract class ItemView extends LinearLayout {
+	private TextView labelView;
+	private View component;
+	private String field;
+	private int group;
 
 	public ItemView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+
+		initView(context);
 	}
 
-	@SuppressLint("NewApi")
-	public ItemView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
+	private void initView(Context context) {
+		this.labelView = this.createLabel(context, null);
+		this.addView(labelView);
+
+		this.component = this.createElement(null);
+		this.addView(component);
+	}
+
+	public void parse(JSONObject config) {
+
+	}
+
+	public void prepareValidator() {
+
+	}
+
+	public boolean validate() {
+		return true;
+	}
+
+	public TextView createLabel(Context context, Form form) {
+		TextView labelView = new TextView(context);
+		labelView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+		return labelView;
+	}
+
+	public abstract View createElement(Form form);
+
+	public abstract String getValue();
+
+	public abstract void setValue();
+
+	public abstract Map<String, String> getKeyValue();
+
+	public abstract void setKeyValue(Map<String, String> keyValue);
+
+	public void setLabel(CharSequence label) {
+		this.labelView.setText(label);
+	}
+
+	public CharSequence getLabel() {
+		return this.labelView.getText();
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public int getGroup() {
+		return group;
+	}
+
+	public void setGroup(int group) {
+		this.group = group;
 	}
 }
